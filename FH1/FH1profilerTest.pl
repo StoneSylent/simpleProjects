fd46bc968520c18ab4e5d29f77c9537f32b12005
stonesylent <stone.sylent@gmail.com>
2016-07-12 16:05:52 -0400

    Magic numbers removed

--- FH1/FH1profilerTest.pl
+++ FH1/FH1profilerTest.pl
@@ -6,7 +6,19 @@ use FolderInfo;
 use Cwd;
 use File::Copy;
 
+
+use constant ROOT_FOLDER_NAME => 'A';
+use constant ROOT_FILE_NAME => 0;
+use constant DEPTH => '-';
+use constant MAX_FILE_LENGTH_EXPECTED => 20;
+use constant MAX_FOLDER_LENGTH_EXPECTED => 5;
+use constant TRUE => 1;
+use constant FALSE => 0;
+
 #perl FH1profilerTest.pl <profile_name>
+
+#---------------------------------Main-----------------------------------
+#------------------------------------------------------------------------
 my $profileName = $ARGV[0];
 
 if ( not profileFound() ) {
@@ -19,7 +31,7 @@ chdir $profileName;
 
 my $profile = new FolderInfo(cwd);
 
-rfTravel($profile, "A", '-');
+rfTravel($profile, '', DEPTH);
 
 printf "\nGoodbye %s\n\n", $profileName; 
 
@@ -27,54 +39,60 @@ printf "\nGoodbye %s\n\n", $profileName;
 #------------------------------------------------------------------------
 sub fileRenameTest1 {	
 	my ($folder,$parent,$name,$depth) = @_;
-	printf "*****renaming the files of %s*****\n",$parent;
+	printf "_______renaming the files of %s_____\n",$parent;
 	while ($f = $folder->nextFile())	{
-		print $depth; #$folder->printInfo($f);
-		printf "%s%s renamed to %s%s\n",$depth,commonSize($f,20),$parent,$name++;
+		printf "%s%s renamed to %s%s\n",$depth,commonSize($f,MAX_FILE_LENGTH_EXPECTED),$parent,$name++;
 	}
-	printf "*****end of files from %s*****\n",$parent;
+	printf "_______end of files from %s_________\n\n\n",$parent;
 }
 
+
+#Does the bulk of the traveling around directories.
+#Recursively travels deeper into the supplied folder.
 sub rfTravel {
 	my ($folder,$parent, $depth) = @_;
-	my $name = "A";
+	
+	# Rename the files first before going deeper!
+	fileRenameTest1($folder,$parent,0,$depth);
+	
+	my $name = ROOT_FOLDER_NAME;
 	my $path = cwd;
 	
 	while ($f = $folder->nextFolder()) {
-		print $depth; #$folder->printInfo($f);
-		printf "%s%s renamed to %s\n",$depth,commonSize($f,20), $parent.$name;
+		my $nName = $parent.$name;
+		printf "%s%s renamed to %s\n",$depth,commonSize($f,MAX_FOLDER_LENGTH_EXPECTED), $nName;
+		#go deeper into the folder 
 		opendir(CHILD, $f);
 		chdir CHILD;
 		closedir(CHILD);
 		#we must go deeper!
-		rfTravel( new FolderInfo(cwd), $parent.$name, $depth.$depth);
+		rfTravel( new FolderInfo(cwd), $nName, $depth.DEPTH);
 		#bubble back up!
 		chdir $path;
 		$name++;
 	}
-	
-	fileRenameTest1($folder,$parent,0,$depth);
-
 }
 
+#resize $text to length $size>0
 sub commonSize {
 	my ( $text, $size ) = @_;
 	my $space = " ";
-	while ( length $text != $size )
+	while ( length $text <= $size )
 	{
 		$text .= $space;
 	}
 	return $text;
 }
 
+#checks the profile folder for 
 sub profileFound {
 	opendir(PROFILES, 'profiles') || die "Missing the profiles folder!\n";
 	while( ( $name = readdir(PROFILES) ) ) {
 		   if ( $name eq $profileName ) {
 		   	closedir(PROFILES);
-		   	return 1;
+		   	return TRUE;
 		   }
 	}
 	closedir(PROFILES);
-	return 0;
+	return FALSE;
 }	
