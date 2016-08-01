fd7504ef6a57017ec0ddb127591cd13445b41ee7
StoneSylent <stone.sylent@gmail.com>
2016-08-01 15:22:38 -0400

    init, contact page change, registration requires
    changes restore commit/b32861ff1bb34a9b11a3b82eb8b4906bb6372ccb

---
+++ WebTest/ASP_MVC_1/MVC_test1/MVC_test1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_test1.Startup))]
+namespace MVC_test1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
