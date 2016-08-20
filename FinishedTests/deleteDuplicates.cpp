31a33e8fec032a0b339cf45e5d04ce96cd2ab9c8
stonesylent <stone.sylent@gmail.com>
2016-08-20 16:18:40 -0400

    prototypes, removal of length

--- FinishedTests/deleteDuplicates.cpp
+++ FinishedTests/deleteDuplicates.cpp
@@ -3,45 +3,50 @@
 	Elements of Programming Interviews by Aziz,Lee,Prakash
 	
 	$ g++ -std=c++11 deleteDuplicates.cpp
+	
+	Removes duplicate values from a sorted vector
+	deleted values are set to zero and pushing to
+	the end of the vector 
+	
 */
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-void printVector ( vector<int> arg, int length) {
-	for (int i=0; i<length; i++)
+void printVector ( vector<int> arg );
+vector<int> deleteDuplicates(vector<int> A);
+
+int main()
+{
+	vector<int> values = {1,1,4,5,6,6,12,12,21,21};
+	cout << "Starting array was ";
+	printVector(values);
+	cout << "  Ending array was ";
+	printVector(deleteDuplicates(values));
+}
+
+void printVector ( vector<int> arg ) {
+	for (int i=0; i<arg.size(); i++)
 	{
 		cout << arg[i] << " ";
 	}
-	cout << "\n";
+	cout << endl;
 }
 
-vector<int> deleteDuplicates(vector<int> A, int length)
+vector<int> deleteDuplicates(vector<int> A)
 {
 	vector<int> temp = A;
-	int writeIndex = 0;
-	for (int i=1; i<length; i++) {
+	int writeIndex = 0;	
+	for (int i=1; i<A.size(); i++) {
 		if ( temp[writeIndex] != temp[i] ) {
 			 temp[++writeIndex] = temp[i];
 		}  
 	}
 	//set duplicates to zero
-	for (int j=writeIndex+1; j<length; j++) {
+	for (int j=writeIndex+1; j<A.size(); j++) {
 		temp[j] = 0;	
 	}
-	length = writeIndex;
 	return temp;
-}
-
-
-int main()
-{
-	int length = 10;
-	vector<int> values = {1,4,5,5,6,6,12,12,21,21};
-	cout << "Starting array was ";
-	printVector(values, length);
-	cout << "  Ending array was ";
-	printVector(deleteDuplicates(values, length), length);
 }
 
