b254cb96b8a52751013f1dee5e41faab9db2b61f
stonesylent <stone.sylent@gmail.com>
2016-08-31 13:14:39 -0400

    body links redirect to new page (excluding #.* targets)

--- javascripts/index.js
+++ javascripts/index.js
@@ -3,5 +3,7 @@ function generate() {
 	var withTime = d.toLocaleString();
 	var temp = document.getElementsByName("timestamp")[0].innerHTML;
 	document.getElementsByName("timestamp")[0].innerHTML = temp+"</br>"+withTime;
+	//links open in new tab, excluding # targets
+	$('a:not([href="#"])').attr("target","_blank");
 };
 
