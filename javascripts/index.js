c283deaa954b96f146bb3c33e5ccccd593aea94d
StoneSylent <stone.sylent@gmail.com>
2016-09-01 10:53:56 -0400

    footer.html init

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,9 +1,8 @@
 function generate() {
-	var d = new Date();
+    var d = new Date();
 	var withTime = d.toLocaleString();
 	var temp = document.getElementsByName("timestamp")[0].innerHTML;
-	document.getElementsByName("timestamp")[0].innerHTML = temp+"</br>"+withTime;
-	//links open in new tab, excluding # targets
-	$('a:not([href="#"])').attr("target","_blank");
+	document.getElementsByName("timestamp")[0].innerHTML = temp+"<br />"+withTime;
+	//links open in new tab, excluding # targets inside content
+	$('div#content a:not([href="#"])').attr("target","_blank");
 };
-
